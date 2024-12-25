25844b859c1e67474e37ac1a560fb8500fed0628
amit013021 <amit013021@gmail.com>
2024-12-25 23:47:01 +0530

    add project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+ 
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+     <div>
+       <h1>Hello, Vite!</h1>
+       <Button className='bg-red-200'>Click Me</Button>
+     </div>
+  )
+}
+
+export default App
